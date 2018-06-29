476971da88e164dcbc6c680be81652b1ec9f5a54
igorfratel <igorfratelsantana@gmail.com>
2018-06-29 14:51:46 -0300

    added accordion

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,26 +1,55 @@
 <template>
-  <div class="planejamento">
-    <h1>Planejamento</h1>
-	<div class="flex-container wrap">
-    <!-- Example of Vue using some data -->
-    <ul v-for="disciplina of disciplinas" :key="disciplina.id_disciplina">
-        <course :code="disciplina.codigo_disciplina" status="cursada"/>
-       <!-- <li>{{ disciplina.codigo_disciplina }}</li> -->
-    </ul>
-	</div>
-  </div>
+
+  <div id="app">
+    <article class="message" :class="accordionClasses">
+
+    <div class="message-header" @click="toggleAccordion()">
+        <p>Planejamento</p>
+    </div>
+
+    <div class="message-body">
+        <div class="flex-container wrap">
+            <!-- Example of Vue using some data -->
+            <ul v-for="disciplina of disciplinas" :key="disciplina.id_disciplina">
+                <course :code="disciplina.codigo_disciplina" status="cursada"/>
+                <!-- <li>{{ disciplina.codigo_disciplina }}</li> -->
+            </ul>
+        </div>
+    </div>
+    </article>
+    </div>
 </template>
 
+
 <script>
-import Course from '@/components/Course.vue'; 
+import Course from '@/components/Course.vue';
+
 export default {
-    components: {
-        Course
-    },
+  components: {
+    Course
+  },
+
   data() {
     return {
-      disciplinas: []
+      disciplinas: [],
+      isOpen: true
     };
+  },
+
+  methods: {
+    toggleAccordion: function() {
+      this.isOpen = !this.isOpen;
+    }
+  },
+
+  computed: {
+    accordionClasses: function() {
+      return {
+        'is-closed': !this.isOpen,
+        'is-primary': this.isOpen,
+        'is-dark': !this.isOpen
+      };
+    }
   },
 
   // Initialize elements in 'data' before rendering page.
@@ -34,39 +63,33 @@ export default {
 </script>
 
 <style lang="scss">
-.planejamento {
-}
+
 .flex-container {
+  display: flex;
   padding: 0;
   margin: 0;
   list-style: none;
   border: 1px solid silver;
   -ms-box-orient: horizontal;
-  display: -webkit-box;
-  display: -moz-box;
-  display: -ms-flexbox;
-  display: -moz-flex;
-  display: -webkit-flex;
-  display: flex;
 }
 
-.nowrap  { 
+.nowrap  {
   -webkit-flex-wrap: nowrap;
   flex-wrap: nowrap;
 }
 
-.wrap    { 
+.wrap    {
   -webkit-flex-wrap: wrap;
   flex-wrap: wrap;
-}  
+}
 .wrap li {
   background: gold;
 }
 
-.wrap-reverse         { 
+.wrap-reverse         {
   -webkit-flex-wrap: wrap-reverse;
   flex-wrap: wrap-reverse;
-}  
+}
 .wrap-reverse li {
   background: deepskyblue;
 }
@@ -77,11 +100,33 @@ export default {
   width: 200px;
   height: 100px;
   margin: 10px;
-  
+
   line-height: 100px;
   color: white;
   font-weight: bold;
   font-size: 2em;
   text-align: center;
+}
+
+.message {
+    max-width: 500px;
+    margin-left: auto;
+    margin-right: auto;
+}
+
+.message-header {
+    cursor: pointer;
+    color: blue;
+}
+
+.message-body   {
+    padding: 0;
+    max-height: 10em;
+    overflow: hidden;
+    transition: 0.3s ease all;
+}
+
+.is-closed .message-body {
+    max-height: 0;
 }
 </style>
