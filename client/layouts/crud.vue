aaad20c6abf8c5debb10f41cfa83d2a2b897b8c7
Caio Ramos <caioramos97@gmail.com>
2018-07-02 10:29:38 -0300

    Começando cruds, desabilitar (ou implementar) autenticacao para testar

---
+++ client/layouts/crud.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="page">
+	  <h1> Crud </h1>
+    <main class="main">
+      <nuxt/>
+    </main>
+  </div>
+</template>
+
+<script>
+
+export default {
+
+};
+</script>
+
+<style lang="scss">
+html {
+  font-family: 'Roboto', sans-serif;
+  font-size: 16px;
+}
+
+.page {
+	width: calc(100vw - (100vw - 100%));
+  height: 100vh;
+
+  /* Grid to position the content */
+  display: grid;
+
+  grid-template-rows: 1fr 10fr 1fr;
+  grid-template-columns: 3fr 9fr;
+
+  grid-template-areas:
+    "aside navbar"
+    "aside main  "
+    "aside footer";
+}
+
+.main {
+  /* Grid to position the cell itself */
+  grid-area: main;
+  justify-self: stretch;
+
+  /* Flexbox to align the content in the cell */
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: center;
+
+  /* FIXME: Border to highlight */
+  border: 8px solid red;
+}
+</style>
---
+++ client/pages/disciplinas/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class=".cruds__index">
+	  <h1> Disciplinas </h1>
+	  <ul v-for="disciplina of disciplinas" :key="disciplina.id_disciplina">
+
+		  <li>
+			  <div>
+			  {{ disciplina.codigo_disciplina }}
+			  <button > Editar </button>
+			  <button > Deletar </button>
+			  </div>
+		  </li>
+	  </ul>
+	  <button> Adicionar </button>
+  </div>
+</template>
+
+<script>
+export default {
+	layout: 'crud',
+  data() {
+    return {
+      disciplinas: [],
+    };
+  },
+
+  async asyncData({ app }) {
+    return {
+      disciplinas: await app.$axios.$get("/disciplinas")
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.cruds__index {
+}
+</style>
